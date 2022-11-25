12ff4ea01aba59444ea90b5fe1258292cdce855c
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2022-11-25 15:59:30 +0530

    Add files via upload

--- src/main/java/com/ty/credential_manager/configuration/Config.java
+++ src/main/java/com/ty/credential_manager/configuration/Config.java
@@ -1,12 +1,24 @@
 package com.ty.credential_manager.configuration;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.ty.credential_manager"})
-public class Config {
-
+      public class Config {
+	
+	@Bean(name="emf")
+	@Scope(value="prototype")
+	public EntityManagerFactory getEntityManagerFactory()
+	{
+		return Persistence.createEntityManagerFactory("cred");
+	}
+	
 }
